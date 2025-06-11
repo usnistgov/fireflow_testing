e95d60d2ce38d2c63e2302da3791c6152a2c8267
ndwarshuis <ndwar@yavin4.ch>
2025-06-11 18:14:04 -0400

    ENH read data for accuri file

--- workflow/scripts/python/test.py
+++ workflow/scripts/python/test.py
@@ -6,7 +6,44 @@ from pathlib import Path
 def main(smk: Any):
     i = Path(smk.input[0])
     o = Path(smk.output[0])
-    pf.py_read_fcs_raw_text(i)
+    opts = next(
+        (
+            x["options"]
+            for x in smk.config["test_files"]
+            if x["name"] == smk.wildcards.testname
+        )
+    )
+    try:
+        vo = None
+        match opts["version_override"]:
+            case "FCS2.0":
+                vo = pf.Version.FCS2_0
+            case "FCS3.0":
+                vo = pf.Version.FCS3_0
+            case "FCS3.1":
+                vo = pf.Version.FCS3_1
+            case "FCS3.2":
+                vo = pf.Version.FCS3_2
+            case _:
+                assert False, "invalid version"
+
+        opts["version_override"] = vo
+    except KeyError:
+        pass
+
+    try:
+        x = opts["text_data_correction"]
+        opts["text_data_correction"] = (x[0], x[1])
+    except KeyError:
+        pass
+
+    try:
+        x = opts["data_correction"]
+        opts["data_correction"] = (x[0], x[1])
+    except KeyError:
+        pass
+
+    pf.fcs_read_std_dataset(i, **opts)
     o.touch()
 
 
