d81a22dceafd70e7617bbb3b1c8d6c48ae6a12ab
njd2 <njd2@nist.gov>
2026-05-23 19:33:39 -0400

    ENH dump diagnostics after reading

--- workflow/scripts/python/pyreflow/test_read.py
+++ workflow/scripts/python/pyreflow/test_read.py
@@ -1,3 +1,5 @@
+import base64
+import json
 from typing import Any
 from pathlib import Path
 import logging
@@ -7,17 +9,27 @@ logging.basicConfig(filename=snakemake.log[0], level=logging.DEBUG)  # type: ign
 logging.captureWarnings(True)
 
 
+def encode_bytes(obj: Any) -> dict[str, Any]:
+    if isinstance(obj, bytes):
+        return {"__bytes__": True, "data": base64.b64encode(obj).decode("ascii")}
+    raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
+
+
 def main(smk: Any) -> None:
     i = Path(smk.input[0])
-    o = Path(smk.output["flag"])
+    flag_out = Path(smk.output["flag"])
+    dump_out = Path(smk.output["dump"])
     repo = RepoType(smk.wildcards.repo)
     id = smk.wildcards.id
     testname = smk.wildcards.testname
     conf = smk.config.find_file_options(repo, testname, id).merged_conf
 
-    core, _ = conf.read_std_dataset(i)
-    o.touch()
+    core, uncore = conf.read_std_dataset(i)
+    flag_out.touch()
     core.write_dataset(smk.output["fcs"])
+
+    with open(dump_out, "w") as f:
+        json.dump(uncore.dict, f, default=encode_bytes)
 
 
 main(snakemake)  # type: ignore
