362a52a242e0cc68ff69202adc499a9e5bdb6a2f
njd2 <njd2@nist.gov>
2026-05-25 18:38:39 -0400

    ADD functional lib

---
+++ workflow/scripts/python/common/functional.py
@@ -0,0 +1,23 @@
+from typing import TypeVar, Callable
+
+X = TypeVar("X")
+Y = TypeVar("Y")
+
+
+def maybe(default: Y, f: Callable[[X], Y], x: X | None) -> Y:
+    return default if x is None else f(x)
+
+
+def fmap_maybe(f: Callable[[X], Y], x: X | None) -> Y | None:
+    return maybe(None, f, x)
+
+
+def key_maybe(xs: dict[X, Y], key: X) -> Y | None:
+    try:
+        return xs[key]
+    except KeyError:
+        return None
+
+
+def esc(s: str) -> str:
+    return s.replace("\n", "\\n")
